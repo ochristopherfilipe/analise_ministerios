072c481e1a5c693d63385ffae9be34e629331721
Christopher Santos <112826117+ochristopherfilipe@users.noreply.github.com>
2025-03-08 20:42:47 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# Database credentials
+DB_HOST = "145.223.92.209"  # Host Externo
+DB_PORT = "5432"  # Porta Externa
+DB_NAME = "postgresql"  # Nome do Banco de Dados
+DB_USER = "postgres"  # Usuário
+DB_PASSWORD = "ZAvbW7c67IKjNF"  # Senha
+
+# Administrator credentials
+ADMIN_USERNAME = "EDILENE SANTOS"
+ADMIN_PASSWORD = "PASTORAEDILENE"
+
+# Map of ministry leaders
+MINISTRY_LEADERS = {
+    "Milaf": "Wendel",
+    "Midaf": "Marcela",
+    "Técnica": "Isaac",
+    "Comunicação": "Marcus",
+    "Intercessão": "Moises",
+    "Introdutores": "Mario"
+} 
